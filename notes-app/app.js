e70674f036ac9624efb3fc19631252dd1c6c348b
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-12 20:24:24 -0400

    Finished 3.12

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,11 @@
 const getNotes = require("./notes");
 const validator = require("validator");
+const chalk = require("chalk");
 
 const msg = getNotes();
 
 console.log(msg);
 console.log(validator.isEmail("me@me.com"));
 console.log(validator.isURL("https://mead.io"));
+console.log(chalk.black.bgGreen("SUCCESS"));
+console.log(chalk.bold.inverse("BOLD AND INVERSED"));
