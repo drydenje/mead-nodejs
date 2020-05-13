04cb7dc1b8355559de1786c0d138e546a2b6c0ef
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-12 22:29:49 -0400

    Finished 4.16

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,14 +1,44 @@
-const getNotes = require("./notes");
-const validator = require("validator");
 const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes");
 
-const msg = getNotes();
-const command = process.argv[2];
+// Customize yargs version
+yargs.version("1.1.0");
 
-console.log("Args:", process.argv);
+// Create 'add' command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note");
+  },
+});
 
-if (command === "add") {
-  console.log("Adding note");
-} else if (command === "remove") {
-  console.log("Removing note!");
-}
+// Create 'remove' command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
+
+// Create 'list' command
+yargs.command({
+  command: "list",
+  describe: "List the notes",
+  handler: function () {
+    console.log("Listing out all notes");
+  },
+});
+
+// Create 'read' command
+yargs.command({
+  command: "read",
+  describe: "Read the notes",
+  handler: function () {
+    console.log("Reading a note");
+  },
+});
+
+console.log(yargs.argv);
