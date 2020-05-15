02a35d9b7bcf268f2e85db2544ffa88a6181f8ac
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-15 18:40:24 -0400

    Finished 4.31

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log("Starting");
+const request = require("request");
+const url =
+  "http://api.weatherstack.com/current?access_key=e02ad3bc30c07b6deec2837377f925dc&query=37.8267,-122.4233";
 
-setTimeout(() => {
-  console.log("2 Second Timer");
-}, 2000);
-
-setTimeout(() => {
-  console.log("0 Second Timer");
-}, 0);
-
-console.log("Stopping");
+request(url, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
