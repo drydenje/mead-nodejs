f0709ca4b0f8c759bb17dd5a36c6497437e714b0
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-17 20:09:51 -0400

    Finished 4.36

---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=pk.eyJ1IjoibWF0c29zIiwiYSI6ImNrYTh0cXNrbTAwNXQycnBoNnY1Y2tnOTgifQ.f5OxrVclDzoKyUK5Auo7bQ`;
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to location services", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find location", undefined);
+    } else {
+      const location = {
+        name: response.body.features[0].place_name,
+        long: response.body.features[0].center[0],
+        lat: response.body.features[0].center[1],
+      };
+      callback("", location);
+    }
+  });
+};
+
+module.exports = geocode;
