6d5137e6b986322b00f69d1d82956cc1201d4eaf
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-19 17:19:15 -0400

    Finished 4.49

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,20 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates");
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("/", (req, res) => {
@@ -27,6 +35,7 @@ app.get("/help", (req, res) => {
   res.render("help", {
     title: "Help",
     message: "Here are some helpful suggestions",
+    name: "Andrew Mead",
   });
 });
 
