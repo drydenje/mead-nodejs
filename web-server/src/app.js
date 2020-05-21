2b5f4be8a2c9c8b305f0303cfc3e17326e948fe4
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-21 13:27:14 -0400

    Finished 4.55

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,9 @@ const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
 const app = express();
 
 // Define paths for Express config
@@ -48,13 +51,23 @@ app.get("/weather", (req, res) => {
     });
   }
 
-  const weatherResponse = {
-    location: address,
-    forecast: "It's snowing",
-    temperature: 48,
-  };
+  geocode(address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error });
+    }
 
-  res.send(weatherResponse);
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address,
+      });
+    });
+  });
 });
 
 app.get("/products", (req, res) => {
