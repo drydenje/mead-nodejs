8e3b2e587a998a2046591db4f9fde680f11c4051
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-21 15:34:50 -0400

    Finished 4.57

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
 console.log("Client side javascript file is loaded");
+
+// fetch("http://puzzle.mead.io/puzzle").then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+fetch("http://localhost:3000/weather?address=Toronto").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      return console.log(data.error);
+    }
+
+    console.log(data);
+  });
+});
