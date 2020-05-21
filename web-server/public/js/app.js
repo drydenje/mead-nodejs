371e428a6532a4d43f4a8357f39ad3564b31be8c
Jeremy Dryden <jeremy.dryden@gmail.com>
2020-05-21 16:45:39 -0400

    Finished 4.58

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,24 @@
-console.log("Client side javascript file is loaded");
+const getWeather = (location = "") => {
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          return console.log(data.error);
+        }
 
-// fetch("http://puzzle.mead.io/puzzle").then((response) => {
-//   response.json().then((data) => {
-//     console.log(data);
-//   });
-// });
-
-fetch("http://localhost:3000/weather?address=Toronto").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      return console.log(data.error);
+        console.log(data);
+      });
     }
+  );
+};
 
-    console.log(data);
-  });
+const weatherForm = document.querySelector("#searchForm");
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  getWeather(location);
 });
